c290ee903b16a5cb2cfb688ca2d4c647554c5dcb
handongyexingren <1340551309@qq.com>
2022-01-16 23:56:25 +0800

    completed，但有点bug，明天改

--- lsh/main.c
+++ lsh/main.c
@@ -5,6 +5,107 @@
 #include <stdio.h>
 #include <string.h>
 
+// 内建命令声明
+int lsh_cd(char **args);
+int lsh_help(char **args);
+int lsh_exit(char **args);
+
+// 内建命令列表
+char *builtin_str[] = {
+    "cd",
+    "help",
+    "exit"
+};
+
+// 内建命令对应的函数列表
+int (*builtin_func[]) (char **) = {
+    &lsh_cd,
+    &lsh_help,
+    &lsh_exit,
+};
+
+// 返回内建命令数量
+int lsh_num_builtins() 
+{
+    return sizeof(builtin_str) / sizeof(char *);
+}
+
+// cd 命令
+// 返回1,继续executing
+int lsh_cd(char **args)
+{
+    if (args[1] == NULL) {
+        fprintf(stderr, "lsh: expected argument to \"cd\"\n");
+    } else {
+        if (chdir(args[1]) != 0) {
+            perror("lsh");
+        }
+    }
+    return 1;
+}
+
+int lsh_help(char **args)
+{
+    printf("hdyxr's LSH\n");
+    printf("Type program names and arguments, and hit enter.\n");
+    printf("The following are built in:\n");
+
+    for(int i = 0; i < lsh_num_builtins(); i++) {
+        printf(" %s\n", builtin_str[i]);
+    }
+
+    printf("Use the man command for information on other program.\n");
+    return 1;
+}
+
+int lsh_exit(char **args)
+{
+    return 0;
+}
+
+// 启动一个程序并等待其结束
+int lsh_launch(char **args)
+{
+    pid_t pid;
+    int status;
+
+    pid = fork();
+    if(pid == 0) {
+        // child process
+        if(execvp(args[0], args) == -1) {
+            perror("lsh");
+        }
+        exit(EXIT_FAILURE);
+    } else if (pid < 0) {
+        // error
+        perror("lsh");
+    } else {
+        do {
+            waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+
+    return 1;
+}
+
+
+// 执行shell内建命令或者启动程序
+int lsh_execute(char **args)
+{
+    if (args[0] == NULL) {
+        // 空命令
+        return 1;
+    }
+
+    for (int i = 0; i < lsh_num_builtins(); i++) {
+        if(strcmp(args[0], builtin_str[i]) == 0) {
+            return (*builtin_func[i])(args);
+        }
+    }
+
+    return lsh_launch(args);
+}
+
 // read a line of input from stdin
 // return the line from stdin
 char * lsh_read_line(void)
@@ -21,6 +122,8 @@ char * lsh_read_line(void)
             exit(EXIT_FAILURE);
         }
     }
+
+    return line;
 #else
 #define LSH_RL_BUFSIZE 1024
     int bufsize = LSH_RL_BUFSIZE;
@@ -43,6 +146,7 @@ char * lsh_read_line(void)
             exit(EXIT_SUCCESS);
         } else if(c == '\n') {
             buffer[position] = '\0';
+            return buffer;
         } else {
             buffer[position] = c;
         }
@@ -58,6 +162,44 @@ char * lsh_read_line(void)
         }
     }
 #endif
+}
+
+// 解析读取行
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM "\t\r\n\a"
+char **lsh_split_line(char *line)
+{
+    int bufsize = LSH_TOK_BUFSIZE;
+    int position = 0;
+    char **tokens = malloc(bufsize * sizeof(char*));
+    char *token;
+
+    if (!tokens) {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(line, LSH_TOK_DELIM);
+    while (!token) {
+        tokens[position] = token;
+        position++;
+
+        if (position >= bufsize) {
+            bufsize += LSH_TOK_BUFSIZE;
+            tokens = realloc(tokens, bufsize * sizeof(char*));
+
+            if (!tokens) {
+                fprintf(stderr, "lsh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        token = strtok(NULL, LSH_TOK_DELIM);
+    }
+
+    // 作为结束的标记
+    tokens[position] = NULL;
+    return tokens;
 }
 
 // loop getting input and executing it.
